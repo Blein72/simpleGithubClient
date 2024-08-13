2013efb5026b28fa5a662bff6ade37029e65b7ef
Blein72 <dolgopolovnik@yandex.ru>
2024-08-13 09:02:42 +0900

    Fixed dependencies for db module

--- local-db/build.gradle.kts
+++ local-db/build.gradle.kts
@@ -47,9 +47,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
 
     // room
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation ("androidx.room:room-ktx:2.6.1")
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation (libs.androidx.room.ktx)
 
     // hilt
     implementation(libs.hilt.android)
